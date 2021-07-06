4ec0fcc642f96d8a213bd6b1ef9176e270c60382
madsterest <odea.madeleine@gmail.com>
2021-07-06 17:02:11 +1000

    Added to server

--- server.js
+++ server.js
@@ -19,6 +19,9 @@ app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 app.listen(PORT, () => {
